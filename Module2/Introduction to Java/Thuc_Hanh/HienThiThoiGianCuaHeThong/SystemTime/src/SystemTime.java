0cb434b059983107bd74c69b4d64bcdb795856d4
leechoncaao <hathaykhongbanghayhat18@gmail.com>
2020-11-28 09:05:26 +0700

    updated

---
+++ Module2/Introduction to Java/Thuc_Hanh/HienThiThoiGianCuaHeThong/SystemTime/src/SystemTime.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        var date = new Date();
+
+        System.out.println("now is " + date);
+    }
+}
