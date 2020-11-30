c07450249f7654bb0ff64f734ada44ff8c6aead8
leechoncaao <hathaykhongbanghayhat18@gmail.com>
2020-11-30 11:22:10 +0700

    completed mang va phuong thuc java, minesweeper not completed !

---
+++ Module2/MangVaPhuongThuc/Thuc_Hanh/src/TemperatureConverter.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("1. Fahrenheit to Celsius");
+        System.out.println("2. Celsius to Fahrenheit");
+        System.out.println("3. Exit ");
+
+        int choice = -1;
+        int temperature;
+        while (choice != 3) {
+            System.out.println("Your choice : ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter a temperature by Fahrenheit :");
+                    temperature = scanner.nextInt();
+
+                    System.out.println("Temperature by Celcius : " + toCelcius(temperature));
+
+                    break;
+                case 2:
+                    System.out.println("Enter a temperature by Celcius :");
+                    temperature = scanner.nextInt();
+
+                    System.out.println("Temperature by Fahrenheit : " + toFahrenheit(temperature));
+
+                    break;
+                default:
+                    System.out.println("Invalid input");
+            }
+        }
+
+    }
+
+    public static double toCelcius(int temperature) {
+        return (5.0 / 9) * (temperature - 32);
+    }
+
+    public static double toFahrenheit(int temperature) {
+        return (9.0 / 5) * temperature + 32;
+    }
+
+}
