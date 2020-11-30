5beec6de40577de5ec2e241c3862a0fe622ccb00
leechoncaao <hathaykhongbanghayhat18@gmail.com>
2020-11-30 14:41:44 +0700

    updated

--- Module2/src/introductionToJava/bai_tap/ConvertCurrency.java
+++ Module2/src/introductionToJava/bai_tap/ConvertCurrency.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class ConvertCurrency {
     public static void main(String[] args) {
-        final int rate = 23000;
+        final int RATE = 23000;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter USD : ");
         var usd = scanner.nextInt();
 
-        var vnd = usd*rate;
+        var vnd = usd* RATE;
 
         System.out.println("Money in VND : " + vnd);
     }
