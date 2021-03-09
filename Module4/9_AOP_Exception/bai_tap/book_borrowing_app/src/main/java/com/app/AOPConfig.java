4ec3d8d27565bd5c80df7a83ac9b155fe3870e8d
leechoncaao <hathaykhongbanghayhat18@gmail.com>
2021-03-09 08:49:09 +0700

    update

--- Module4/9_AOP_Exception/bai_tap/book_borrowing_app/src/main/java/com/app/AOPConfig.java
+++ Module4/9_AOP_Exception/bai_tap/book_borrowing_app/src/main/java/com/app/AOPConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AOP_config {
+public class AOPConfig {
     @Bean
     public Logger logger(){
         return new Logger();
